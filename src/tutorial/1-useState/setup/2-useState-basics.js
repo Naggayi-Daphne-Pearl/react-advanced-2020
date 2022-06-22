88c5f450917600699ed3ba31fec81ff7ccd4def7
Naggayi Daphne <daphnepearl101@gmail.com>
2022-06-22 12:49:26 +0300

    userState basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // daphne is the default value. 
+  const[text, setText] = useState('daphne'); 
+  const handleClick =() =>{
+    if (text === 'daphne') {
+      setText('naggayi');
+    } else {
+      setText('daphne');
+    }
+  }
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>Change title</button>
+    </React.Fragment>
+
+  )
 };
 
 export default UseStateBasics;
