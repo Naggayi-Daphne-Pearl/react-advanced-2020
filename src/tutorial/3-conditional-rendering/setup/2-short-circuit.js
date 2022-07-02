cf515dbb73593f774c7671a4a10291d97348647f
Naggayi Daphne <daphnepearl101@gmail.com>
2022-07-02 14:01:04 +0300

    tenary operators

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,16 +4,31 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const[text, setText] = useState('')
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false)
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
   return <>
   {/* <h1> {firstValue}</h1>
   <h2>value : {secondValue}</h2> */}
+  
   {/* || if false display john doe */}
   <h1>{text || 'john doe'}</h1>
-  {/* if true  display hello world  */}
-  {text && <h1>hello world</h1>}
+  <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+
+  {/* if error is true display error...*/}
+  {isError && <h1>error...</h1>}
+
+  {/* We do not use if because it doesnt return a value */}
+  {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
   </>
 };
 
